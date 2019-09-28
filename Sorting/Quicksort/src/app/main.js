f92dc9c5924686a27142a8f177a14e8f93da7938
T1F3 <tife1379@gmail.com>
2019-09-28 20:48:13 +0100

    m

--- Sorting/Quicksort/src/app/main.js
+++ Sorting/Quicksort/src/app/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
     // $("button").click(function(){
     //     $('#4').insertBefore('#1');
     // });
@@ -8,31 +7,32 @@ $(document).ready(function(){
     var vals
 
     $("#gen").click(function(){
-        $('.list').empty();
         text = $('#txt_name').val();
         vals = text.split(",");
         build_arr(vals)
     });
 
     function build_arr(arr){
+        $('#txt_name').val("");
         var pre_text = '<div class="list-index" id="'
         var res = ""
         var i;
         for (i = 0; i < arr.length; i++) {
           res += pre_text + i + '">' + arr[i] +'</div>' ;
         }
+        $('.list').empty();
         $( ".list" ).append(res);
-        $('#txt_name').val("");
+
     }
 
     $("#sort").click(function(){
         Quicksort(vals)
+        alert(vals)
         build_arr(vals)
     });
 
     function Quicksort(arr) {
-        Quicksort_h(arr, 0, arr.length)
-        return arr
+        Quicksort_h(arr, 0, arr.length - 1)
     }
 
     function Quicksort_h(arr, start_ind, end_ind){
@@ -42,14 +42,13 @@ $(document).ready(function(){
         var piv_index = pivot(arr, start_ind, end_ind)
         Quicksort_h(arr, start_ind, piv_index - 1)
         Quicksort_h(arr, piv_index + 1, end_ind)
-        
     }
 
     function pivot(arr, start_ind, end_ind){
         piv = arr[end_ind]
         piv_index = start_ind;
         var i = 0;
-        for (i = start_ind; start_ind < end_ind; i++){
+        for (i = start_ind; i < end_ind; i++){
             if (arr[i] < piv){
                 arr[i], arr[piv_index] =  arr[piv_index], arr[i]
                 piv_index++
